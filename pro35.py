7641e52565cc83d8303a687e02db3d33849b6f1b
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-08-16 14:05:17 +0530

    Update pro35.py

--- pro35.py
+++ pro35.py
@@ -1,21 +1,21 @@
-cad=input()
-l21=list(set(cad))
-xens=1
+vg=input()
+l1=list(set(vg))
+xen1=1
 antt=0
 check=False
 while True:
-    ch=l21[antt]
-    for y in range(0,len(cad)-xens):
-        if ch in cad[y:y+xens]:
+    ch=l1[antt]
+    for y in range(0,len(vg)-xen1):
+        if ch in vg[y:y+xen1]:
             check=True
         else:
             check=False
             antt=antt+1
             if antt>=len(l1):
              antt=0
-             xens=xens+1
+             xen1=xen1+1
             break
 
     if check==True:
         break
-print(xens)
+print(xen1)
